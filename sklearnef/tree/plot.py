a30e7621c6a1994d2c7478de69f1129e25c24ad6
Oskar Maier <oskar.maier@googlemail.com>
2015-05-19 17:26:56 +0200

    Finished plotting script for sherwood files.

--- sklearnef/tree/plot.py
+++ sklearnef/tree/plot.py
@@ -1,72 +1,123 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import random
 import scipy.stats
 import numpy as np
 from tree import UnSupervisedDecisionTreeClassifier
+import matplotlib.pyplot as plt
+from matplotlib.mlab import griddata
 
+# ----- Constants -----
 distance = 10
 sigma = 0.4
-n_samples = 1000
+resolution = .5
+n_samples = 5000
 n_features = 2
 n_clusters = 5
+#np.random.seed(0)
+
+# ----- Clusters -----
 means = [[i * distance] * n_features for i in range(n_clusters)]
 means = []
 for i in range(n_clusters):
     means.append([])
     for j in range(n_features):
         means[i].append(random.randint(0, distance))
-cov = np.diag([sigma] * n_features)
-cov[np.triu_indices(n_features, 1)] = sigma/2
-cov[np.tril_indices(n_features, -1)] = sigma/2
+cov = np.diag([np.random.random() * sigma for _ in range(n_features)])
+n_tri_elements = (n_features * (n_features - 1)) / 2
+cov[np.triu_indices(n_features, 1)] = [np.random.random() * sigma/2 for _ in range(n_tri_elements)]
+cov[np.tril_indices(n_features, -1)] = [np.random.random() * sigma/2 for _ in range(n_tri_elements)]
 
-#!TODO: Should I fix the random state?
+# ----- Sample train set -----
 X_train = np.concatenate([scipy.stats.multivariate_normal.rvs(mean, cov, n_samples) for mean in means])
-X_test = np.concatenate([scipy.stats.multivariate_normal.rvs(mean, cov, n_samples) for mean in means])
-y_test = np.sum([scipy.stats.multivariate_normal.pdf(X_test, mean, cov) for mean in means], 0)
-y_test_log = np.sum([scipy.stats.multivariate_normal.logpdf(X_test, mean, cov) for mean in means], 0)
 
+# ----- Grid -----
+x_lower = X_train[:,0].min() - 2 * sigma
+x_upper = X_train[:,0].max() + 2 * sigma
+y_lower = X_train[:,1].min() - 2 * sigma
+y_upper = X_train[:,1].max() + 2 * sigma
+grid = np.mgrid[x_lower:x_upper:resolution,y_lower:y_upper:resolution]
+
+
+# ----- Training -----
 clf = UnSupervisedDecisionTreeClassifier(random_state=0)
 clf.fit(X_train)
-prob_predict = clf.predict_proba(X_test)
-    
-import matplotlib.pyplot as plt
-from matplotlib.mlab import griddata
 
-x = X_test[:,0]
-y = X_test[:,1]
+# ----- Prediction -----
+X_test_pred = np.rollaxis(grid, 0, 3).reshape((np.product(grid.shape[1:]), grid.shape[0]))
+prob_predict = clf.predict_proba(X_test_pred)
 
-z_min = min(prob_predict.min(), y_test.min())
-z_max = max(prob_predict.max(), y_test.max())
+# ----- Ground truth -----
+X_test_gt = np.rollaxis(grid, 0, 3)
+prob_gt = np.sum([scipy.stats.multivariate_normal.pdf(X_test_gt, mean, cov) for mean in means], 0)
 
-# first plot
+# ----- Plotting -----
+x, y = grid
+x = np.unique(x)
+y = np.unique(y)
+
+# first plot: gt
 plt.subplot(2, 1, 1)
-xi = np.linspace(x.min(),x.max(),100)
-yi = np.linspace(y.min(),y.max(),100)
-zi = griddata(x,y,y_test,xi,yi,interp='linear')
+CS = plt.contour(x,y,prob_gt.T,15,linewidths=0.5,colors='k')
+CS = plt.contourf(x,y,prob_gt.T,15,cmap=plt.cm.PiYG)
+plt.colorbar()
 
-CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
-CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.PiYG)
-plt.colorbar() # draw colorbar
-
-#plt.scatter(x,y,marker='o',c='b',s=5)
 plt.xlim(min(x),max(x))
 plt.ylim(min(y),max(y))
 plt.title('GT')
 
-# second plot
+# second plot: prediction
 plt.subplot(2, 1, 2)
-xi = np.linspace(x.min(),x.max(),100)
-yi = np.linspace(y.min(),y.max(),100)
-zi = griddata(x,y,prob_predict,xi,yi,interp='linear')
+CS = plt.contour(x,y,prob_predict.reshape((x.size,y.size)).T,15,linewidths=0.5,colors='k')
+CS = plt.contourf(x,y,prob_predict.reshape((x.size,y.size)).T,15,cmap=plt.cm.PiYG)
+plt.colorbar()
 
-CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
-CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.PiYG)
-plt.colorbar() # draw colorbar
-
-#plt.scatter(x,y,marker='o',c='b',s=5)
 plt.xlim(min(x),max(x))
 plt.ylim(min(y),max(y))
-plt.title('learned')
+plt.title('Prediction')
 
 plt.show()
+
+
+### OLDER ###
+#X_test = np.concatenate([scipy.stats.multivariate_normal.rvs(mean, cov, n_samples) for mean in means])
+#y_test = np.sum([scipy.stats.multivariate_normal.pdf(X_test, mean, cov) for mean in means], 0)
+#y_test_log = np.sum([scipy.stats.multivariate_normal.logpdf(X_test, mean, cov) for mean in means], 0)
+
+#x = X_test[:,0]
+#y = X_test[:,1]
+
+#z_min = min(prob_predict.min(), y_test.min())
+#z_max = max(prob_predict.max(), y_test.max())
+
+# first plot
+#plt.subplot(2, 1, 1)
+#xi = np.linspace(x.min(),x.max(),100)
+#yi = np.linspace(y.min(),y.max(),100)
+#zi = griddata(x,y,y_test,xi,yi,interp='linear')
+
+#CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
+#CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.PiYG)
+#plt.colorbar() # draw colorbar
+
+#plt.scatter(x,y,marker='o',c='b',s=5)
+#plt.xlim(min(x),max(x))
+#plt.ylim(min(y),max(y))
+#plt.title('GT')
+
+# second plot
+#plt.subplot(2, 1, 2)
+#xi = np.linspace(x.min(),x.max(),100)
+#yi = np.linspace(y.min(),y.max(),100)
+#zi = griddata(x,y,prob_predict,xi,yi,interp='linear')
+
+#CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
+#CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.PiYG)
+#plt.colorbar() # draw colorbar
+
+#plt.scatter(x,y,marker='o',c='b',s=5)
+#plt.xlim(min(x),max(x))
+#plt.ylim(min(y),max(y))
+#plt.title('learned')
+
+#plt.show()
---
+++ sklearnef/tree/plot_sherwood.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+"""Plot an example of Microsoft sherwood library."""
+
+# build-in modules
+import argparse
+
+# third-party modules
+import numpy
+import matplotlib.pyplot as plt
+
+# path changes
+
+# own modules
+from tree import UnSupervisedDecisionTreeClassifier
+
+# information
+__author__ = "Oskar Maier"
+__version__ = "r0.1.0, 2015-05-18"
+__email__ = "oskar.maier@googlemail.com"
+__status__ = "Release"
+__description__ = """
+Plot an example of Microsoft sherwood library.
+"""
+
+# code
+def main():
+    args = getArguments(getParser())
+    
+    # parse dataset
+    data = numpy.genfromtxt(args.dataset)
+    if not 2 == data.ndim:
+        raise Exception("Can only plot 2D datasets.")
+    
+    # train forest
+    clf = UnSupervisedDecisionTreeClassifier(random_state=0)
+    clf.fit(data)
+    
+    # generate plot grid
+    xrange = (data[:,0].min(), data[:,0].max())
+    yrange = (data[:,1].min(), data[:,1].max())
+    xresolution = (xrange[1] - xrange[0]) / float(args.resolution)
+    yresolution = (yrange[1] - yrange[0]) / float(args.resolution) 
+    
+    grid = numpy.mgrid[xrange[0]:xrange[1]:xresolution,yrange[0]:yrange[1]:yresolution]
+    x, y = grid
+    x = numpy.unique(x)
+    y = numpy.unique(y)
+    
+    # apply forest
+    X = numpy.swapaxes(grid.reshape(2, numpy.product(grid.shape[1:])), 0, 1)
+    z = clf.predict_proba(X)
+    z = z.reshape(grid.shape[1:])
+
+    # plot
+    plt.subplot(2, 1, 1)
+    CS = plt.contour(x,y,z.T,15,linewidths=0.5,colors='k')
+    CS = plt.contourf(x,y,z.T,100,cmap=plt.cm.PiYG)
+    plt.colorbar()
+    
+    plt.scatter(data[:,0], data[:,1])
+    
+    plt.xlim(min(x), max(x))
+    plt.ylim(min(y), max(y))
+    plt.title('Learned density distribution of: {}'.format(args.dataset))
+    
+    plt.show()
+
+def getArguments(parser):
+    "Provides additional validation of the arguments collected by argparse."
+    return parser.parse_args()
+
+def getParser():
+    "Creates and returns the argparse parser object."
+    parser = argparse.ArgumentParser(description=__description__)
+    parser.add_argument('dataset', help='One of the sharewood density example datasets (a text file containing a table).')
+    #parser.add_argument('output', help='Target volume.')
+    parser.add_argument('-r', '--resolution', dest='resolution', type=int, default=100, help='Plot resolution (points-per-dimension).')
+    #parser.add_argument('-v', dest='verbose', action='store_true', help='Display more information.')
+    #parser.add_argument('-d', dest='debug', action='store_true', help='Display debug information.')
+    #parser.add_argument('-f', dest='force', action='store_true', help='Silently override existing output images.')
+    return parser
+
+if __name__ == "__main__":
+    main()
